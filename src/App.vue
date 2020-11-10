81d1c66e9f123f5df3c05066d95c4e7cb0b203be
klart <zhangxhklart@163.com>
2020-11-10 21:03:35 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+<div>hi</div>
+<klart />
+</template>
+
+<script>
+import klart from './components/klart.vue'
+
+export default {
+    name: 'App',
+    components: {
+        kalrt: kalrt
+    }
+}
+</script>
