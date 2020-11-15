23c51a14deb49f794a5221770bae867303d42d92
klart <zhangxhklart@163.com>
2020-11-15 22:42:37 +0800

    button 添加动画

--- src/Lib/Switch.vue
+++ src/Lib/Switch.vue
@@ -1,8 +1,26 @@
 <template>
-<button>
+<button @click="toggle" :class="{checked}">
     <span></span>
 </button>
 </template>
+
+<script lang="ts">
+import {
+    ref
+} from 'vue'
+export default {
+    setup() {
+        const checked = ref(true)
+        const toggle = () => {
+            checked.value = !checked.value
+        }
+        return {
+            checked,
+            toggle
+        }
+    }
+}
+</script>
 
 <style lang="scss" scoped>
 $h:22px;
@@ -12,7 +30,7 @@ button {
     height: $h;
     width: $h*2;
     border: none;
-    background: blue;
+    background: gray;
     border-radius: $h/2;
     position: relative;
 }
@@ -25,5 +43,18 @@ span {
     width: $h2;
     background: white;
     border-radius: $h2/2;
+    transition: left 250ms;
+}
+
+button.checked {
+    background: blue;
+}
+
+button.checked>span {
+    left: calc(100% - #{$h2} - 2px);
+}
+
+button:focus {
+    outline: none;
 }
 </style>
