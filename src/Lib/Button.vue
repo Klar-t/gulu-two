a45b380b115316d6c89757abf125b22f7ceefb42
klart <zhangxhklart@163.com>
2020-11-16 21:12:08 +0800

    添加theme

---
+++ src/Lib/Button.vue
@@ -0,0 +1,23 @@
+<template>
+<button class="gulu-button" :class="`theme-${theme}`">
+    <slot />
+</button>
+</template>
+
+<script lang="ts">
+export default {
+    props: {
+        theme: {
+            type: String,
+            default: 'button'
+        }
+
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+div {
+    border: 1px solid red;
+}
+</style>
--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,5 +1,27 @@
 <template>
+<div>button 实例
+</div>
 <div>
-    Button组件
+    <Button>你好</Button>
+    <Button theme="button">按钮</Button>
+    <Button theme="link">链接</Button>
+    <Button theme="text">文本</Button>
 </div>
 </template>
+
+<script lang="ts">
+import Button from '../Lib/Button.vue'
+export default {
+    components: {
+        Button
+    },
+    setup() {
+        const onClick = () => {
+            console.log('hi')
+        }
+        return {
+            onClick
+        }
+    }
+}
+</script>
