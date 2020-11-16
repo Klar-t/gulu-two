558ede09fa8977164d7f958f77110b62f0fd63f9
klart <zhangxhklart@163.com>
2020-11-16 23:55:01 +0800

    添加Dialog组件

---
+++ src/Lib/Dialog.vue
@@ -0,0 +1,24 @@
+<template>
+<div>Dialog 示例</div>
+<h1>示例一</h1>
+<div class="dialog-wrapper">
+    <header>标题</header>
+    <main>
+        <p>第一行字</p>
+        <p>第二行字</p>
+    </main>
+    <footer>
+        <Button>OK</Button>
+        <Button>Cancel</Button>
+    </footer>
+</div>
+</template>
+
+<script>
+import Button from '../Lib/Button.vue'
+export default {
+    components: {
+        Button
+    }
+}
+</script>
--- src/components/DiaLogDemo.vue
+++ src/components/DiaLogDemo.vue
@@ -2,4 +2,17 @@
 <div>
     DiaLogDemo组件
 </div>
+<Dialog />
+<div>
+
+</div>
 </template>
+
+<script lang="ts">
+import Dialog from '../Lib/Dialog.vue'
+export default {
+    components: {
+        Dialog
+    }
+}
+</script>
