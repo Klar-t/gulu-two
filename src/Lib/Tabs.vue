06b01db1d6c1d99f09a0ae0c2409c4caea7b6e45
klart <zhangxhklart@163.com>
2020-11-18 22:55:25 +0800

    添加Tabs

---
+++ src/Lib/Tabs.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        Tabs组件
+    </div>
+</template>
+<script lang="ts">
+export default {
+    setup(props,context){
+        console.log({...context.slots.default()[0]})
+        console.log({...context.slots.default()[1]})
+    }
+}
+</script>
--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,5 +1,21 @@
 <template>
 <div>
-    Tabs组件
+    Tabs示例
 </div>
+<h1>示例1</h1>
+<Tabs>
+    <Tab title="导航1">内容1</Tab>
+    <Tab title="导航2">内容2</Tab>
+</Tabs>
 </template>
+
+<script lang="ts">
+import Tabs from '../Lib/Tabs.vue'
+import Tab from '../Lib/Tab.vue'
+export default {
+    components:{
+        Tabs,
+        Tab
+    }
+}
+</script>
