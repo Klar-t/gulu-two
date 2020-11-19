a6dae96093e3e7646efccf5529aa58ccc87defcc
klart <zhangxhklart@163.com>
2020-11-19 23:34:30 +0800

    检查子标签是否符合标准

--- src/Lib/Tabs.vue
+++ src/Lib/Tabs.vue
@@ -1,13 +1,38 @@
 <template>
-    <div>
-        Tabs组件
+    <div class="gulu-tabs">
+        <div class="gulu-tabs-nav">
+            <div class="gulu-tabs-nav-item"
+            :class="{selected:t===selected}"
+            v-for="(t,index) in titles" :key="index">{{t}}</div>
+        </div>
+        <div class="gulul-tabs-content">
+            <component v-for="(c,index) in defaults" :is="c" :key="index"/>
+        </div>
+        
     </div>
 </template>
 <script lang="ts">
+import Tab from './Tab.vue'
 export default {
+    props:{
+        selected:{
+            type:String
+        }
+    },
     setup(props,context){
-        console.log({...context.slots.default()[0]})
-        console.log({...context.slots.default()[1]})
+        const defaults=context.slots.default()
+        defaults.forEach((tag)=>{
+            if(tag.type!=Tab){
+                throw new Error('Tabs 子标签必须是Tab')
+            }
+        })
+        const titles=defaults.map((tag)=>{
+            return tag.props.title
+        })
+        return {
+            defaults,titles
+        }
     }
+    
 }
 </script>
--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -3,7 +3,7 @@
     Tabs示例
 </div>
 <h1>示例1</h1>
-<Tabs>
+<Tabs selected="导航1">
     <Tab title="导航1">内容1</Tab>
     <Tab title="导航2">内容2</Tab>
 </Tabs>
