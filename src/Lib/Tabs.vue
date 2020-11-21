6c3bdd1618cc735c1dd86666f59cf83f1abb424d
klart <zhangxhklart@163.com>
2020-11-21 21:10:40 +0800

    使用CSS切换Tab标签

--- src/Lib/Tabs.vue
+++ src/Lib/Tabs.vue
@@ -2,16 +2,19 @@
     <div class="gulu-tabs">
         <div class="gulu-tabs-nav">
             <div class="gulu-tabs-nav-item"
-            :class="{selected:t===selected}"
+            @click="select(t)"
+            :class="{selected: t===selected}" 
             v-for="(t,index) in titles" :key="index">{{t}}</div>
         </div>
         <div class="gulul-tabs-content">
-            <component v-for="(c,index) in defaults" :is="c" :key="index"/>
+            <component class="gulu-tabs-content-item"
+            :class="{selected:c.props.title===selected}" v-for="c in defaults" :is="c"/>
         </div>
         
     </div>
 </template>
 <script lang="ts">
+import { computed } from 'vue'
 import Tab from './Tab.vue'
 export default {
     props:{
@@ -21,18 +24,62 @@ export default {
     },
     setup(props,context){
         const defaults=context.slots.default()
+        console.log(...defaults)
         defaults.forEach((tag)=>{
-            if(tag.type!=Tab){
+            if(tag.type!=Tab){ 
                 throw new Error('Tabs 子标签必须是Tab')
             }
         })
         const titles=defaults.map((tag)=>{
             return tag.props.title
         })
+        const current=computed(()=>{
+            console.log() 
+            return defaults.filter((tag)=>{
+                return tag.props.title===props.selected
+            })[0]
+        }) 
+        
+        const select = (title: string) => {
+            context.emit('update:selected', title)
+            console.log("d")
+        }
         return {
-            defaults,titles
+            defaults,titles,select,current
         }
     }
     
 }
 </script>
+<style lang="scss">
+    $blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.gulu-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+    &-item{
+        display: none;
+        &.selected{
+            display: block;
+        }
+    }
+  }
+}
+</style>
--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -3,7 +3,7 @@
     Tabs示例
 </div>
 <h1>示例1</h1>
-<Tabs selected="导航1">
+<Tabs v-model:selected="x">
     <Tab title="导航1">内容1</Tab>
     <Tab title="导航2">内容2</Tab>
 </Tabs>
@@ -12,10 +12,17 @@
 <script lang="ts">
 import Tabs from '../Lib/Tabs.vue'
 import Tab from '../Lib/Tab.vue'
+import { ref } from 'vue'
 export default {
     components:{
         Tabs,
         Tab
+    },
+    setup(){
+        const x=ref('导航1')
+        return {
+            x
+        }
     }
 }
 </script>
