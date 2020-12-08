7ddb0358dad59cc9c7b30bc849ec0a40ff104884
klart <zhangxhklart@163.com>
2020-12-08 19:40:33 +0800

    更新路由，修改aside遮挡

--- src/components/DocDemo.vue
+++
@@ -1,5 +0,0 @@
-<template>
-<div>
-    DocDemo组件
-</div>
-</template>
--- src/router.ts
+++ src/router.ts
@@ -2,13 +2,9 @@ import {createWebHashHistory,createRouter} from 'vue-router'
 import Home from './views/Home.vue'
 import Doc from './views/Doc.vue'
 import SwitchDemo from "./components/SwitchDemo.vue"
-import DocDemo from "./components/DocDemo.vue"
 import ButtonDemo from "./components/ButtonDemo.vue"
 import DiaLogDemo from "./components/DiaLogDemo.vue"
 import TabsDemo from "./components/TabsDemo.vue"
-import Intro from "./views/Intro.vue"
-import GetStarted from './views/GetStarted.vue'
-import Install from './views/Install.vue'
 import Markdown from './components/Markdown.vue'
 import { h } from 'vue'
 
@@ -19,7 +15,7 @@ export const router=createRouter({
     routes:[
         {path:'/',component:Home},
         {path:'/doc',component:Doc,children:[
-            {path:'',component:DocDemo},
+            {path:'',redirect:'/doc/intro'},
             {path:'Intro',component:md('intro')},
             {path:'get-started',component:md('get-started')},
             {path:'install',component:md('install')},
