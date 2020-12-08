e376f25f5b5700b31e3b6c574e243d23d83d9c37
klart <zhangxhklart@163.com>
2020-12-08 21:40:16 +0800

    解决build不能加载md文件问题，md下划线消除

--- src/router.ts
+++ src/router.ts
@@ -7,18 +7,21 @@ import DiaLogDemo from "./components/DiaLogDemo.vue"
 import TabsDemo from "./components/TabsDemo.vue"
 import Markdown from './components/Markdown.vue'
 import { h } from 'vue'
+import intro from './markdown/intro.md'
+import getStarted from './markdown/get-started.md'
+import install from './markdown//install.md'
 
 const history=createWebHashHistory()
-const md=filename=>h(Markdown,{path:`../markdown/${filename}.md`,key:filename})
+const md= string =>h(Markdown,{content:string,key:string})
 export const router=createRouter({
     history:history,
     routes:[
         {path:'/',component:Home},
         {path:'/doc',component:Doc,children:[
             {path:'',redirect:'/doc/intro'},
-            {path:'Intro',component:md('intro')},
-            {path:'get-started',component:md('get-started')},
-            {path:'install',component:md('install')},
+            {path:'Intro',component:md(intro)},
+            {path:'get-started',component:md(getStarted)},
+            {path:'install',component:md(install)},
             {path:'switch',component:SwitchDemo},
             {path:'button',component:ButtonDemo},
             {path:'dialog',component:DiaLogDemo},
