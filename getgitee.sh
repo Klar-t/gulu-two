ab60e6461a2f405ad3194126dd5d536e5152b3fc
klart <zhangxhklart@163.com>
2020-12-09 21:47:36 +0800

    添加gitee仓库

---
+++ getgitee.sh
@@ -0,0 +1,10 @@
+rm -rf dist &&
+yarn build &&
+cd dist 
+git init &&
+git add .  &&
+git commit -m "update commit" &&
+git branch -M main &&
+git remote add origin https://gitee.com/Klart/gulu-ui-website.git &&
+git push -f -u origin main &&
+cd ..
